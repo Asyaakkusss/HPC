0d07ac09a7d9301c39a8c58cfcfe0c0f132b7dc1
Jakob Danninger <68190922+jdanninger@users.noreply.github.com>
2024-12-04 16:24:36 -0500

    gradient boost add

--- Gradient_Boost.py
+++ Gradient_Boost.py
@@ -2,6 +2,7 @@ import xgboost as xgb
 import pandas as pd
 import os
 print(xgb.__version__)
+import numpy as np
 
 def read_smr_file(file_path):
     records = {}
@@ -13,7 +14,7 @@ def read_smr_file(file_path):
             record_name = parts[0].strip()
             premature = parts[4].strip() == 't'
             early = parts[5].strip() == 't'
-            records[record_name] = premature or early
+            records[record_name] = premature
     return records
 
 def is_premature(record_name, records):
@@ -22,12 +23,13 @@ def is_premature(record_name, records):
 # Example usage
 file_path = '/Users/jdanninger/Documents/GitHub/HPC/term-preterm-ehg-database-1.0.1/tpehgdb.smr'
 records = read_smr_file(file_path)
-print(records)
 record_name = 'tpehg1007'
 premature = is_premature(record_name, records)
 print(f'{record_name} is premature: {premature}')
 # Load all the data into an array
 csv2_path = "/Users/jdanninger/Documents/GitHub/HPC/CSV2"
+
+
 
 def load_csvs_to_dict(csv2_path):
     dataframes = {}
@@ -35,8 +37,24 @@ def load_csvs_to_dict(csv2_path):
         if filename.endswith('.csv'):
             file_path = os.path.join(csv2_path, filename)
             df = pd.read_csv(file_path)
-            # only keep the columns ["1", "2", "3"]
-            df = df[["1"]]
+            # only keep columns "1" "2" and "3"
+
+            # df = df[[ "1", "2", "3"]]
+
+
+            # for cols ["1", "2", "3"] create new cols ["1 fft", "2 fft", "3 fft"] which are col 1 but with fft applied
+            # df = df["1"]
+
+            for col in ["1", "2", "3"]:
+                df[col + " fft"] = np.fft.fft(df[col])
+                # the FFT is a complex number, so we need to split it into real and imaginary parts
+                df[col + " fft real"] = df[col + " fft"].apply(lambda x: x.real)
+                # drop anything iwth imaginary part
+                df = df.drop(columns=[col + " fft"])
+
+
+
+
             key = os.path.splitext(filename)[0]  # Use filename without extension as key
             dataframes[key] = df
     return dataframes
@@ -90,7 +108,7 @@ X = combined_df.drop(columns=['record_name'])
 y = labels
 
 # Split the data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
 # Train the gradient boosting model
 model = xgb.XGBClassifier()
@@ -102,5 +120,14 @@ y_pred = model.predict(X_test)
 # Evaluate the model
 accuracy = accuracy_score(y_test, y_pred)
 print(f'Accuracy: {accuracy}')
+
+from sklearn.metrics import roc_auc_score
+
+# Make predictions
+y_pred_proba = model.predict_proba(X_test)[:, 1]
+
+# Evaluate the model
+auc = roc_auc_score(y_test, y_pred_proba)
+print(f'AUC: {auc}')
 
 
